c8c9b3225c135059a1e5d5f49677b6ae0b16b6b3
baxiyi <mzw5810@gmail.com>
2019-11-02 23:08:35 +0800

    shelf,rule

--- src/pages/Rule/delete/index.js
+++ src/pages/Rule/delete/index.js
@@ -1,8 +1,103 @@
-import React from 'react'
-export default function() {
-  return (
-    <div>
-      rule/delete
-    </div>
-  )
+import React, {PureComponent} from 'react'
+import {Input, Table} from 'antd'
+import './index.css'
+
+const {Search} = Input;
+
+export default class extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ruleId: '',
+      deviceId: '',
+    }
+  }
+
+  searchByRuleId(value) {
+    console.log('search rule id ' + value)
+    this.setState({
+      ruleId: value,
+    }, () => this.updateRuleList)
+  } 
+
+  searchByServiceId(value) {
+    console.log('search device id '+ value)
+    this.setState({
+      deviceId: value,
+    }, () => this.updateRuleList())
+  }
+
+  updateRuleList() {
+    console.log('update rule list')
+  }
+
+  onOrStop(status) {
+    console.log(status);
+  }
+
+  deleteRule(id) {
+    console.log('delete rule ' + id);
+  }
+
+  render() {
+    const columns = [
+      {
+        title: '规则ID',
+        dataIndex: 'ruleId',
+      },
+      {
+        title: '警告信息',
+        dataIndex: 'warningInfor',
+      },
+      {
+        title: '关联的设备',
+        dataIndex: 'devices',
+      },
+      {
+        title: '状态',
+        dataIndex: 'status',
+      },
+      {
+        title: '',
+        dataIndex: 'onOrStop',
+        render: (value, record) => (
+          <a onClick={this.onOrStop(record.status)}>
+            {record.status == '已启用' ? '停用' : '启用'}
+          </a>
+        )
+      },
+      {
+        title: '',
+        dataIndex: 'deleteRule',
+        render: (value, record) => (<a onClick={() => this.deleteRule(record.ruleId)}>删除</a>)
+      }
+    ];
+    const tableData = [
+      {key: '1', }
+    ];
+    return (
+      <div className="rule-delete">
+        规则ID：
+        <Search 
+          placeholder="请输入规则ID"
+          style={{width: 200}}
+          onSearch={(value) => this.searchByRuleId(value)}
+          className="rule-search-input"
+        />
+        设备ID：
+        <Search 
+          placeholder="请输入设备ID"
+          style={{width: 200}}
+          onSearch={(value) => this.searchByServiceId(value)}
+        />
+        <Table 
+          columns={columns}
+          dataSource={tableData}
+          bordered
+          className="rule-table"
+          pagination={false}
+        />
+      </div>
+    )
+  }
 }
--- src/pages/Rule/insert/index.js
+++ src/pages/Rule/insert/index.js
@@ -1,8 +1,84 @@
-import React from 'react'
-export default function() {
-  return (
-    <div>
-      rule/insert
-    </div>
-  )
+import React, { PureComponent } from 'react'
+import {Input, Table, Button} from 'antd'
+import './index.css'
+
+const {TextArea} = Input;
+
+export default class extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ruleId: '',
+      warningInfor: '',
+      tableData: [],
+    }
+  }
+
+  addTableRow() {
+    console.log('add table row');
+  }
+
+  saveTableData() {
+    console.log('save table data')
+  }
+
+  render() {
+    const columns = [
+      {
+        title: '序号',
+        dataIndex: 'seq',
+      },
+      {
+        title: '设备1的ID',
+        dataIndex: 'device1Id',
+      },
+      {
+        title: '设备2的ID',
+        dataIndex: 'device2Id',
+      },
+      {
+        title: '关系类型',
+        dataIndex: 'relationType',
+      }
+    ];
+
+    return (
+      <div className="rule-insert">
+        <div className="id-input">
+          规则ID：
+          <Input
+            style={{width: 200}}
+            placeholder="请输入规则ID"
+          />
+        </div>
+        <div className="rule-infor">
+          告警信息：
+          <TextArea 
+            style={{width: 800}}
+            placeholder="请输入告警信息"
+            rows={4}
+            className="my-textArea"
+          />
+        </div>
+        <Table 
+          columns={columns}
+          dataSource={this.state.tableData}
+          bordered
+          className="rule-table"
+        />
+        <Button type="primary" onClick={() => this.addTableRow()}>
+          + 新增警告项
+        </Button>
+        <div className="save-button">
+          <Button
+            type="primary"
+            size="large"
+            onClick={() => this.saveTableData()}
+          >
+            确认添加
+          </Button>
+        </div>
+      </div>
+    )
+  }
 }
